b27bee919b145b5443333b056f900c322a8a2407
Mayank2177 <153661521+Mayank2177@users.noreply.github.com>
2025-02-18 16:38:46 +0530

    Update Helloworld.java

--- Basics/Helloworld.java
+++ Basics/Helloworld.java
@@ -1,4 +1,4 @@
-public Helloworld{
+class Helloworld{
   public static void main(String[] args){
     System.out,println("Hello World!");  ///change the printed word by changing the world withing double quotation 
   }
