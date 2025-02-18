ba62847ded6de457f6c75d55317b522388e50721
Mayank2177 <153661521+Mayank2177@users.noreply.github.com>
2025-02-18 16:19:04 +0530

    Create Helloworld.java

---
+++ Basics/Helloworld.java
@@ -0,0 +1,5 @@
+public Helloworld{
+  public static void main(String[] args){
+    System.out,println("Hello World!");  ///change the printed word by changing the world withing double quotation 
+  }
+}
