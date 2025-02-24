78583df5bbcea86203ab93167d3c9268b1a9ce6d
Mayank2177 <153661521+Mayank2177@users.noreply.github.com>
2025-02-24 15:31:11 +0530

    Update Yourname.java

--- Basics/Yourname.java
+++ Basics/Yourname.java
@@ -1,1 +1,11 @@
+import  java.util.Scanner;
 
+class Yourname {
+    public static void main(String[] args) {
+        Scanner var= new Scanner(System.in);
+        System.out.println("Enter your name:");
+        
+        String name= var.nextLine();
+        System.out.println("Your name is "+ name);
+    }
+}
